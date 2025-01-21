980ca9c6a6a12cc738d113c3e8b4d46e16cd20ab
Pranav  Kini <93433640+PK470@users.noreply.github.com>
2025-01-22 03:41:12 +0530

    changes made

--- chat_app/chat_app/asgi.py
+++ chat_app/chat_app/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from chat import consumer
+
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
